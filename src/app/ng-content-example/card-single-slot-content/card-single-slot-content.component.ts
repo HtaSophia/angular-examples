d2004942f44fcf6491b295da30172332534861ef
Aghata Sophia <aghatasophia@gmail.com>
2022-06-06 18:11:22 -0300

    Create Card Single-Slot Content Component

---
+++ src/app/ng-content-example/card-single-slot-content/card-single-slot-content.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-single-slot-content',
+  templateUrl: './card-single-slot-content.component.html',
+  styleUrls: ['./card-single-slot-content.component.scss']
+})
+export class CardSingleSlotContentComponent {}
